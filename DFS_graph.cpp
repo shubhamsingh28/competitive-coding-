2eef4fdc982e578f263bfe151bbe9192df307d2a
Shubham Singh <shubham.singh2882@gmail.com>
2018-07-28 18:55:26 +0530

    Create DFS_graph.cpp

---
+++ DFS_graph.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int v,e;
+	cin>>v>>e;
+	vector<int>V[v];
+	for(int i=0;i<e;i++){
+	    int x,y;
+	    cin>>x>>y;
+	    V[x].push_back(y);
+	    V[y].push_back(x);
+	}
+	//start of BFS
+	int visited[v]={0};
+	stack<int>q;
+	q.push(0);
+	visited[0]=1;
+	while(!q.empty()){
+	    int t=q.top();
+	    q.pop();
+	    cout<<t<<endl;
+	    for(auto it=V[t].begin();it!=V[t].end();it++){
+	        if(visited[*it]==0){
+	            q.push(*it);
+	           visited[*it]=1;
+	        }
+	    }
+	    
+	}
+
+	return 0;
+}
+
